a300279e34d4260779444d9bbea2905ac146d34a
BerkayUnutkanEHB <berkay.unutkan@student.ehb.be>
2024-11-21 09:45:53 +0100

    Initial commit for FitnessTrackerApp

---
+++ FitnessTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerAppApp.swift
+//  FitnessTrackerApp
+//
+//  Created by Berkay Unutkan on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
